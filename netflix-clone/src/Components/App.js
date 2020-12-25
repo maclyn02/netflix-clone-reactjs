6ca6ebd4b190524029077b24f8c121fef5e3efdc
Maclyn Afonso <>
2020-12-25 20:21:34 +0000

    basic homepage and project restructuring

---
+++ netflix-clone/src/Components/App.js
@@ -0,0 +1,34 @@
+import './App.css' 
+import requests from '../requests' 
+import Row from './Row' 
+import tmdb_logo from '../images/tmdb_logo.svg' 
+import Banner from './Banner' 
+
+function App() {
+  return (
+    <div className="App">
+      {/* Navbar */}
+
+      {/* Banner */}
+      <Banner/>
+
+      <Row title='NETFLIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
+      <Row title='TRENDING NOW' fetchUrl={requests.fetchTrending} />
+      <Row title='TOP RATED' fetchUrl={requests.fetchTopRated} />
+      <Row title='ACTION MOVIES' fetchUrl={requests.fetchActionMovies} />
+      <Row title='COMEDY MOVIES' fetchUrl={requests.fetchComedyMovies} />
+      <Row title='HORROR MOVIES' fetchUrl={requests.fetchHorrorMovies} />
+      <Row title='ROMANCE MOVIES' fetchUrl={requests.fetchRomanceMovies} />
+      <Row title='DOCUMENTARIES' fetchUrl={requests.fetchDocumentaries} />
+
+      <div className='footer'>
+        <label>Data source</label>
+        <div>
+          <img src={tmdb_logo} alt='TMDB'></img>
+        </div>
+      </div>
+    </div>
+  ) 
+}
+
+export default App 
